0671f968b0121e95f65cfb63423b200a68d3bd91
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-14 00:49:09 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/*!
+ * base-argv <https://github.com/jonschlinkert/base-argv>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+var extend = require('extend-shallow');
+var yargs = require('yargs');
+
+module.exports = function(argv, options) {
+  var opts = extend({}, options);
+};
---
+++ test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+require('mocha');
+var assert = require('assert');
+var baseArgv = require('./');
+
+describe('base-argv', function() {
+  it('should export a function', function() {
+    assert.equal(typeof baseArgv, 'function');
+  });
+
+  it('should return an object', function() {
+    assert.equal(typeof baseArgv, 'object');
+  });
+
+  it('should throw an error when invalid args are passed', function(cb) {
+    try {
+      baseArgv();
+      cb(new Error('expected an error'));
+    } catch (err) {
+      assert(err);
+      assert.equal(err.message, 'expected a string');
+    }
+  });
+});
