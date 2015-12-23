d4ae44de11329e1a687e21b4c2fc42d1315e4259
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-23 16:26:57 -0500

    minor refactor, break code into smaller functions

--- index.js
+++ index.js
@@ -10,103 +10,87 @@
 var minimist = require('minimist');
 var toTasks = require('./lib/to-tasks');
 var utils = require('./lib/utils');
+var toKeys = require('./lib/keys');
 
 module.exports = function(options) {
   return function(app) {
-    var opts = utils.extend({}, options);
-    if (opts.prop) {
-      opts[opts.prop] = app[opts.prop];
-    }
-
-    opts.commands = app.commands || [];
-
-    // add `keys` from base-config plugin (map-config)
-    if (app.config && Array.isArray(app.config.keys)) {
-      opts.commands = utils.union(opts.commands, app.config.keys);
-    }
-    // add `keys` from base-cli plugin (map-config)
-    if (app.cli && Array.isArray(app.cli.keys)) {
-      opts.commands = utils.union(opts.commands, app.cli.keys);
-    }
-
-    opts.tasks = app.tasks;
-    app.define('processArgv', processArgv(opts));
+    app.define('argv', function(argv) {
+      var opts = toKeys(app, options);
+      return processArgv(argv, opts);
+    });
   };
 };
 
-function processArgv(options) {
-  var opts = utils.extend({}, options);
+function processArgv(argv, options) {
+  var opts = options || {};
   var commands = opts.commands;
   var tasks = opts.tasks;
   var prop = opts.prop;
 
-  return function(argv) {
-    argv = !utils.isObject(argv)
-      ? minimist(utils.arrayify(argv))
-      : argv;
+  if (Array.isArray(argv)) {
+    argv = minimist(utils.arrayify(argv));
+  }
 
-    var res = {};
-    res.orig = utils.extend({}, argv);
-    res._ = [];
-    res.tasks = [];
-    res.commands = {};
-    res.options = {};
-    if (prop) res[prop] = [];
-
-    var appKeys = (prop && opts.hasOwnProperty(prop))
-      ? Object.keys(opts[prop])
-      : [];
-
-    var taskKeys = !Array.isArray(tasks)
-      ? (tasks ? Object.keys(tasks) : [])
-      : tasks;
-
-    var arr = argv._;
-    var len = arr.length, i = -1;
-
-    while (++i < len) {
-      var ele = arr[i];
-
-      if (/,/.test(ele) && !/[.|:]/.test(ele)) {
-        res.tasks = utils.union(res.tasks, ele.split(','));
-        continue;
-      }
-
-      if (isApp(appKeys, ele) || isTask(taskKeys, ele)) {
-        if (prop) {
-          var obj = toTasks(ele, opts, prop);
-          res[prop] = res[prop].concat(obj);
-        } else {
-          res.tasks = utils.union(res.tasks, ele.split(','));
-        }
-        continue;
-      }
-      res._.push(ele);
-    }
-
-    for (var key in argv) {
-      if (key === '_') {
-        continue;
-      }
-
-      var argvArr = utils.arrayify(argv[key]);
-      var expanded = utils.expandArgs(argvArr)[0];
-
-      if (isCommand(commands, key)) {
-        res.commands[key] = expanded;
-      } else {
-        res.options[key] = expanded;
-      }
-    }
-    return res;
+  var res = {
+    orig: utils.extend({}, argv),
+    _: [],
+    tasks: [],
+    commands: {},
+    options: {}
   };
-}
 
-module.exports.processArgv = processArgv;
+  if (typeof prop === 'string') {
+    res[prop] = [];
+  }
+
+  for (var key in argv) {
+    if (key === '_') {
+      res = reduceArray(argv[key], res, prop, opts);
+    }
+    res = reduceOptions(argv[key], key, res, commands);
+  }
+  return res;
+};
 
 /**
  * Utils
  */
+
+function reduceArray(args, res, prop, opts) {
+  var tasks = opts.tasks;
+  var apps = opts[prop];
+
+  var len = args.length, i = -1;
+  while (++i < len) {
+    var ele = args[i];
+
+    if (/,/.test(ele) && !/[.|:]/.test(ele)) {
+      res.tasks = utils.union(res.tasks, ele.split(','));
+      continue;
+    }
+    if (isApp(apps, ele) || isTask(tasks, ele)) {
+      if (prop) {
+        var obj = toTasks(ele, opts, prop);
+        res[prop] = res[prop].concat(obj);
+      } else {
+        res.tasks = utils.union(res.tasks, ele.split(','));
+      }
+      continue;
+    }
+    res._.push(ele);
+  }
+  return res;
+}
+
+function reduceOptions(val, key, res, commands) {
+  var args = utils.expandArgs(utils.arrayify(val))[0];
+  if (isCommand(commands, key)) {
+    res.commands[key] = args;
+  } else {
+    res.options[key] = args;
+  }
+  return res;
+}
 
 function isApp(apps, key) {
   return is(apps, key);
@@ -121,10 +105,20 @@ function isTask(tasks, key) {
 }
 
 function is(arr, key, ch) {
+  console.log(arr)
   if (!arr) return false;
+  if (!Array.isArray(arr)) {
+    return true;
+  }
   if (arr.indexOf(key) > -1) {
     return true;
   }
   var seg = key.split(/\W/).shift();
   return arr.indexOf(seg) > -1;
 }
+
+/**
+ * Expose `processArgv` method
+ */
+
+module.exports.processArgv = processArgv;
